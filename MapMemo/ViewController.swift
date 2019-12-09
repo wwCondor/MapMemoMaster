7b1c6b3b92e2c6915516701244d6661c0a264808
wwCondor <wouter@wearesure.io>
2019-12-09 10:49:13 +0100

    Initial Commit

---
+++ MapMemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapMemo
+//
+//  Created by Wouter Willebrands on 09/12/2019.
+//  Copyright © 2019 Studio Willebrands. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
